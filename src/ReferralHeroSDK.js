3bc17f0b0822738a5e4d9c01d195ab7145b4fb2a
Tobias <tobias@aistechnolabs.org>
2023-08-03 17:21:21 +0530

    Minor Changes 03-08-23

--- src/ReferralHeroSDK.js
+++ src/ReferralHeroSDK.js
@@ -3,7 +3,11 @@ const BASE_URL = 'https://gorest.co.in/public/v2';
 
 export const fetchReferralUserData = async () => {
   try {
-    const response = await fetch(`${BASE_URL}/user`);
+    const response = await fetch(`${BASE_URL}/users`);
+    if (!response.ok) {
+      console.error('Error response:', response.status, response.statusText);
+      throw new Error('Network response was not ok.');
+    }
     const data = await response.json();
     return data;
   } catch (error) {
